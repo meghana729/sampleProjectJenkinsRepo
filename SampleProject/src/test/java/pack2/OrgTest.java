1eda6834006770cb3255796d06bf6351aec31101
meghana729 <meghanar729@gmail.com>
2026-06-10 17:35:42 +0530

    added extra

--- SampleProject/src/test/java/pack2/OrgTest.java
+++ SampleProject/src/test/java/pack2/OrgTest.java
@@ -6,6 +6,15 @@ public class OrgTest {
 
 	@Test
 	public void createorgtest() {
+		System.out.println("execute createcontactTest");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println("url="+URL);
+		System.out.println("browser="+BROWSER);
+		System.out.println("username="+USERNAME);
+		System.out.println("password="+PASSWORD);
 		System.out.println("execute org test");
 	}
 	@Test
