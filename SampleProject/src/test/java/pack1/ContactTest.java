3ef5b0b4f574aff30aea417ee4bab701fd8bce10
KIRAN <KIRAN@DESKTOP-5OB5LTC.mshome.net>
2026-06-10 15:11:21 +0530

    commit 1

---
+++ SampleProject/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest() {
+		System.out.println("execute createcontactTest");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println("url="+URL);
+		System.out.println("browser="+BROWSER);
+		System.out.println("username="+USERNAME);
+		System.out.println("password="+PASSWORD);
+	}
+	@Test
+	public void modifycontacttest() {
+		System.out.println("execute contactmodel");
+	}
+
+}
---
+++ SampleProject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createorgtest() {
+		System.out.println("execute org test");
+	}
+	@Test
+	public void modifyorgtest() {
+		System.out.println("execute modifyorgtest");
+	}
+}
